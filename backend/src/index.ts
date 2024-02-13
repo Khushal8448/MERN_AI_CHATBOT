b3a1d93805641c0f22b0762c15cd0efcd05b1244
Khushal Mali <khushalmali8448@gmail.com>
2024-02-14 00:44:09 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+// Connections and Listeners
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase()
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server Opened & connected to the Database✌️`);
+    })
+  )
+  .catch((error) => console.log(error));
